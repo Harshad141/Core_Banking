db07380950e71f19c97cb7b8162d16135a10a2f2
Harshad Dhane <59340082+Harshad141@users.noreply.github.com>
2021-04-26 13:08:10 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #0f0f0f;"><b>Core-Banking</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar"
+           style="background-color : #0f0f0f;">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #C0392B;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #C0392B;"><b>New account</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #C0392B;"><b>Transfer</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #C0392B;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
